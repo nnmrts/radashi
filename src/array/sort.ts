57d8b4d281b1355006241847f8d492d5c97cf541
Nano Miratus <nanomiratus@gmail.com>
2025-07-26 10:25:59 +0200

    change `sort`'s direction argument to a string

--- src/array/sort.ts
+++ src/array/sort.ts
@@ -12,18 +12,18 @@
  * ]
  *
  * sort(fish, f => f.weight) // => [Bass, Trout, Marlin]
- * sort(fish, f => f.weight, true) // => [Marlin, Trout, Bass]
+ * sort(fish, f => f.weight, "desc") // => [Marlin, Trout, Bass]
  * ```
  */
 export function sort<T>(
   array: readonly T[],
   getter: (item: T) => number,
-  desc = false,
+  direction: 'asc' | 'desc' = 'asc',
 ): T[] {
   if (!array) {
     return []
   }
   const asc = (a: T, b: T) => getter(a) - getter(b)
   const dsc = (a: T, b: T) => getter(b) - getter(a)
-  return array.slice().sort(desc === true ? dsc : asc)
+  return array.slice().sort(direction === 'desc' ? dsc : asc)
 }
